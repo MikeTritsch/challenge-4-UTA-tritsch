0e26ff952ff9c058946518cb367de18d4e680e2d
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-09-30 22:11:34 -0500

    experimented with timer/adding questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,41 @@
 var timeEl = document.querySelector(".timer");
 var startGame = document.querySelector("#startButton");
 var instructions = document.querySelector(".instructions");
-console.log(instructions);
+var gameSpace = document.querySelector(".gamespace");
+var questionDisplay = document.createElement("h1");
+var sampleQuestion = "Testing";
+var questions = [
+    {
+        question: "This is a question",
+        answers: ["a", "b", "c", "d"]
+    },
+
+    {
+        question: "This is another question",
+        answers: ["a", "b", "c", "d"]
+    },
+
+    {
+        question: "This is a third question",
+        answers: ["a", "b", "c", "d"]
+    },
+
+    {
+        question: "This is a fourth question",
+        answers: ["a", "b", "c", "d"]
+    },
+
+    {
+        question: "This is question yurrr",
+        answers: ["a", "b", "c", "d"]
+    }
+]
 
 startGame.addEventListener("click", function() {
     // Removes button and instructions so far
     startGame.style.display = "none";
     instructions.style.display = "none";
+    questionDisplay.textContent = "This is question 1";
 
     // Adds timer
     var secondsLeft = 30;
@@ -19,8 +48,7 @@ startGame.addEventListener("click", function() {
             clearInterval(timerInterval);
             // sendMessage();
         }
-    }, 1000)
+    }, 100)
     }
-
     setTime();
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,22 +5,29 @@ var questions = [
     },
 
     {
-        question: "This is a question",
+        question: "This is another question",
         answers: ["a", "b", "c", "d"]
     },
 
     {
-        question: "This is a question",
+        question: "This is a third question",
         answers: ["a", "b", "c", "d"]
     },
 
     {
-        question: "This is a question",
+        question: "This is a fourth question",
         answers: ["a", "b", "c", "d"]
     },
 
     {
-        question: "This is a question",
+        question: "This is question yurrr",
         answers: ["a", "b", "c", "d"]
     }
 ]
+var gameSpace = document.querySelector(".gamespace");
+
+console.log(questions);
+
+function displayQuestions() {
+    
+}
